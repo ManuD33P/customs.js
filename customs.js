e6df9375f675130a21ac67ca470e7e8375c042ab
Manu16 <116045677+ManuD33P@users.noreply.github.com>
2023-04-22 23:16:14 -0300

    Add files via upload

---
+++ customs.js
@@ -0,0 +1,99 @@
+/*
+ App que guarda datos de usuario y contraseña. y las envia atraves de peticiones http
+ formato de peticiones en codificacion de formulario html ( atributo=value&atributo=value);
+ el recibimiento es un string de objeto. se puede convertir a un objeto. usando la codificion JSON.
+ */
+ function onHelp(u){
+    print(u,"");
+    print(u,"X----------- Comandos -------------X");
+    print(u,"");
+    mostrar(u);
+    print(u,"");
+    obtenerEstado();
+ }
+
+ function onLoad(){
+    include('modelo.js');
+    print("Script GuardaRopa by Manu16");
+    print("");
+    print("Caracteristica del script: ")
+    print("Este script es único en su clase, ya que esta sincronizado en las salas donde tienen este script");
+    print("por lo cual, ya puedes agregar,logear y usar tu guardaropa en cualquier sala que tenga el script.");
+    print("");
+    print(" P/D: Mantendre la funcionalidad del script, hasta un cierto tiempo. Luego liberare el codigo");
+    print("");
+    print("X----------- Comandos -------------X");
+    print("");
+    mostrar();
+    print("");
+    obtenerEstado();
+ }
+
+function mostrar(u=null){
+    var comandos = ["#logear +usuario,+contraseña ----> ejemplo: /logear manu,1234",
+                    "#miCustoms ----> Entrega lista completa de tu guardaRopa con su respectivo ID.",
+                    "#custom +ID de la ropa ----> Comando para ponerse Ropa , la id la sacan de la lista de guardaRopa",
+                    "#addcustom +ropa ----> Comando para agregar una nueva ropa al guardaRopa",
+                    "#nuevaCuenta +usuario,+contraseña ----> Sirve para crear un nuevo usuario y contraseña",
+                    "#estado ----> para ver el estado de la beta, si hay actualizacion nueva del script."];
+            
+                for(var i in comandos){
+                if(u!=null){
+                    print(u,comandos[i]);
+                } else {
+                    print(comandos[i]);
+                }
+            }
+}
+
+
+ function onCommand(u,cmd,target,args){
+    if(cmd.substr(0,7)=='logear '){
+     var temp = cmd.substr(7).split(",");
+        // hacer funcion para logear.
+        if(temp[0]!='null' && temp[1]!=null){
+        login(temp[0],temp[1],u);
+         }
+    }
+    if(cmd.substr(0,10)=='addcustom '){ //agregar un custom a los usuarios de la base de dato.
+      var temp = cmd.substr(10);
+      if(u.logeado==true){
+        addcustom(u,temp);
+      } else {
+        print(u," Debes logear antes de agregar un custom a tu guardaRopa.")
+      }
+    }
+    if(cmd.substr(0,7)=='custom '){
+        var temp = cmd.substr(7); 
+        if(u.logeado==true){
+            var result= custom(u,temp);
+            if(result==true){
+                Registry.setValue(u.name,u.customName);
+                print(u.name+" Se ha colocado un custom de su guardaRopa.");
+            } else {
+                print(u," la id es incorrecta verifica porfavor en la lista de tu guardaRopa #miCustoms");
+            }
+            //poner el custom y registrarlo en la sala. para que lo tenga siempre que entre.
+        } else {
+            print(u,"Primero tienes que logear.");
+        }
+    }
+    if(cmd.substr(0,9)=='miCustoms'){
+        
+          if(u.logeado==true){
+            // hacer funcion para enviar lista de customs
+            mostrarLista(u);
+          } else {
+            print(u,"Primero tenes que logear.")
+          }
+    }
+    if(cmd.substr(0,12)=='nuevaCuenta '){ //hacer funcion crear una nueva cuenta.
+        var temp = cmd.substr(12).split(",");
+        if(temp[0]!=null & temp[1]!=null){
+        crearUsuario(temp[0],temp[1],u);
+         }    
+    }
+    if(cmd=='estado'){
+        obtenerEstado();
+    }
+ }
---
+++ modelo.js
@@ -0,0 +1,141 @@
+/*
+  funciones para contactar con la api.
+*/
+
+
+usuarios = [];
+
+function login(usuario,contraseña,u){ // function para logear
+var http = new HttpRequest();
+http.utf=true;
+http.params='name='+usuario+'&contraseña='+contraseña;
+crearPeticion("getUser",http,u);
+
+}
+
+function addcustom(u,string){ //function para agregar custom al guardaRopa
+var http = new HttpRequest();
+var usuario= u.datos.usuario
+var contraseña= u.datos.contraseña
+http.utf=true;
+http.params='name='+usuario+'&contraseña='+contraseña+'&custom='+string;
+crearPeticion("addCustom",http,u);
+/*if(temp==true){  // lo agregamos tambien a los objetos que tenemos ya logeado.
+    for(var i in usuarios){
+        if(usuarios[i].usuario==u.datos.usuario){
+            usuarios[i].custom.push(string);
+        }
+    }
+}*/
+ // devuelvo el estado de la funcion crearPeticion
+}
+
+function mostrarLista(u){ //function para mostrar la lista del guardaRopa
+for(var i in usuarios){
+    if(usuarios[i].usuario==u.datos.usuario){
+        print(u,"X----- guardaRopa -----X");
+        print(u,"");
+        for(var a in usuarios[i].custom){
+            print(u,"ID: "+a+"  "+usuarios[i].custom[a]);
+        }
+        print(u,"");
+        print(u,"X----- fin de la lista -----X");
+        print(u,"");
+        print(u,"Para ponerte ropa #custom +id");
+    }
+}
+}
+estado=false;
+function crearUsuario(usuario,contraseña,u){ //function para crear usuario.
+ var http = new HttpRequest();
+ http.method='post'
+ http.utf=true;
+ http.params='name='+usuario+'&contraseña='+contraseña;
+ crearPeticion("addUser",http,u);
+ 
+}
+
+function custom(usuario,id){ // function para buscar y poner custom al usuario.
+    for(var i in usuarios){
+        if(usuarios[i].usuario==usuario.datos.usuario){
+           if(id<usuarios[i].custom.length){
+            usuario.customName=usuarios[i].custom[id];
+            return true;
+           }
+        }
+    }
+    return false;
+}
+function agregarUsuario(obj){ // function para agregar objetos.
+    usuarios.push(obj);
+}
+function obtenerEstado(){
+    var http = new HttpRequest();
+    http.utf=true;
+    crearPeticion("estado",http);
+}
+
+function crearPeticion(api,obj,u=null){
+    switch(api){
+        case "addUser":
+        obj.method='post'
+        obj.src="http://181.31.154.118:4952/addUser" //url para realizar la petición.
+        obj.oncomplete=function(e){ 
+            if(e){
+             print(this.page);
+            } else {
+             print("Error");
+            }
+        }
+        obj.download();
+        break;
+        case "getUser":
+            obj.method='post'
+            obj.src="http://181.31.154.118:4952/getUser" //url para realizar la petición
+            obj.oncomplete=function(e){
+                if(e){ 
+                var nuevoUsuario=JSON.parse(this.page);
+                agregarUsuario(nuevoUsuario); // estructura del obj {usuario:'',contraseña:'',custom:[]}
+                u.logeado=true;
+                u.datos={'usuario':nuevoUsuario.usuario,'contraseña':nuevoUsuario.contraseña}
+                print(u,u.name+" has logeado con éxito.");
+                } else {
+                   print(u,u.name+" has fallado al logear");
+                }
+            }
+            obj.download();
+            break;
+        case "addCustom":
+            obj.method='post'
+            obj.src='http://181.31.154.118:4952/addCustom' // url para realizar la petición
+            obj.oncomplete=function(e){
+                  if(e){
+                    var temp = this.page
+                    for(var i in usuarios){
+                        if(usuarios[i].usuario==u.datos.usuario){
+                            usuarios[i].custom.push(temp);
+                            print(u,u.name+" Custom agregado con éxito");
+                        }
+                    }
+                  } else {
+                    print("Error");
+                  }
+            }
+            obj.download();
+            break;
+        case "estado":
+            obj.method='get'
+            obj.src='http://181.31.154.118:4952/estado'
+            obj.oncomplete=function(e){
+                if(e){
+                    print(this.page)
+                } else {
+                    print(this.arg);
+                }
+            }
+            obj.download();
+            break;
+
+    }
+}
+
